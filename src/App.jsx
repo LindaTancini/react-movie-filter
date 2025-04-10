d0f38e94f56d9e2c3c3cff1c4d2478d5e652cafb
LindaTancini <lindatancini95@gmail.com>
2025-04-10 13:34:13 +0200

    feat: add array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import { useState } from "react";
 
+// METTO QUI L'ARRAY DI OGGETTI
+
+const movies = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
+console.log(movies);
+
 function App() {}
 
 export default App;
